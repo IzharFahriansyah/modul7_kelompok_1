92576d4ffb70dfe204012378e41d7f0700ee8d50
CrossFode <149864968+CrossFode@users.noreply.github.com>
2024-04-06 08:54:32 +0700

    Menambahkan class DataMahasiswa

--- Modul7_kelompok 1/Program.cs
+++ Modul7_kelompok 1/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+namespace Modul7_kelompok1
+{
+    public class DataMahasiswa1302220004
+    {
+        public string firstName { set; get; }
+
+        public string lastName { set; get; }
+
+        public string gender { set; get; }
+
+        public int age { set; get; }
+
+        public Address address { set; get; }
+
+        public List<Courses> courses { set; get; }
+
+        public class Address
+        {
+            public string streetAddress { set; get; }
+
+            public string city { set; get; }
+
+            public string state { set; get; }
+        }
+
+        public class Courses
+        {
+            public string code { set; get; }
+
+            public string name { set; get; }
+        }
+
+        public void ReadJSON()
+        {
+            string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            string jsonString = File.ReadAllText(path + "/jurnal7_1_1302220093.json");
+
+            DataMahasiswa1302220004 mhs = JsonSerializer.Deserialize<DataMahasiswa1302220004>(jsonString);
+            Console.WriteLine($"Nama : {mhs.firstName} {mhs.lastName}");
+            Console.WriteLine($"Jenis Kelamin : {mhs.gender}");
+            Console.WriteLine($"Usia : {mhs.age}");
+            Console.WriteLine($"Alamat : {mhs.address.streetAddress}, {mhs.address.city}, {mhs.address.state}");
+            foreach (Courses course in courses)
+            {
+                Console.WriteLine($"Code: {course.code}, Name: {course.name}");
+            }
+        }
+    }
+}
--- Modul7_kelompok 1/obj/Debug/net8.0/Modul7_kelompok 1.AssemblyInfo.cs
+++ Modul7_kelompok 1/obj/Debug/net8.0/Modul7_kelompok 1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Modul7_kelompok 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9db7453d2a7e6260a34a2c82413410171338263d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Modul7_kelompok 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Modul7_kelompok 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
